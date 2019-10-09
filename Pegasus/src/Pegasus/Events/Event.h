9398e84d3b64b038ea77511a851f08e70eaca86b
JensenJ <jensenbradley@icloud.com>
2019-10-09 20:40:35 +0100

    ImGui Events
    Can now interact with ImGui using event system

--- Pegasus/src/Pegasus/Events/Event.h
+++ Pegasus/src/Pegasus/Events/Event.h
@@ -9,7 +9,7 @@ namespace Pegasus {
 		None = 0,
 		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
 		AppTick, AppUpdate, AppRender,
-		KeyPressed, KeyReleased,
+		KeyPressed, KeyReleased, KeyTyped,
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
 
--- Pegasus/src/Pegasus/ImGui/ImGuiLayer.h
+++ Pegasus/src/Pegasus/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Pegasus/Layer.h"
 
+#include "Pegasus/Events/ApplicationEvent.h"
+#include "Pegasus/Events/KeyEvent.h"
+#include "Pegasus/Events/MouseEvent.h"
+
 namespace Pegasus {
 	class PEGASUS_API ImGuiLayer : public Layer {
 	public:
@@ -12,7 +16,15 @@ namespace Pegasus {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
-
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private: 
 		float m_Time = 0.0f;
 	};
